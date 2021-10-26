928510504bce4e60da9199468916c443ebfbb385
Maks <rezakoff.maxim@gmail.com>
2021-10-26 19:44:23 +0300

    Берем пользователей из апи

--- frontend/src/components/Users/User/User.jsx
+++ frontend/src/components/Users/User/User.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import classes from './User.module.css'
+import styles from './User.module.css'
+import userPhoto from '../../../assets/images/User.png'
 
 const User = (props) => {
     debugger;
     return (
         <div>
             <div>
-                <img src={props.user.photoUrl} className={classes.userPhoto} />
+                <img src={ props.user.photos.small != null ? props.user.photos.small : userPhoto } className={styles.userPhoto} />
             </div>
             <div>
-                <div>{props.user.fullName}</div>
+                <div>{props.user.name}</div>
                 <div>{props.user.status}</div>
-                <div>{props.user.location.country}, {props.user.location.city}</div>
+                <div>{"props.user.location.country"}, {"props.user.location.city"}</div>
                 { props.user.followed 
                     ? <button onClick={() => props.toggleFollow(props.user.id)}>Unfollow</button> 
                     : <button onClick={() => props.toggleFollow(props.user.id)}>Follow</button> }                
--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -1,44 +1,13 @@
+import * as axios from 'axios'
 import React from 'react';
 import User from './User/User';
-import classes from './Users.module.css'
+import styles from './Users.module.css'
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            { 
-                id: 1, 
-                followed: false, 
-                fullName: "Dmitry K", 
-                status: "Wassup", 
-                photoUrl: 'https://pbs.twimg.com/profile_images/1799215218/dima2.jpg',
-                location: { 
-                    city: "Minsk",
-                    country: "Belarus"
-                }
-            },
-            {
-                id: 2, 
-                followed: true,
-                fullName: "Benny B", 
-                status: "Teamwork is dream work!", 
-                photoUrl: 'https://www.gamehelper.top/wp-content/uploads/2020/10/bennet-gajd-v-genshin-impact-min.jpg',
-                location: { 
-                    city: "Mondstadt",
-                    country: "Taivat"
-                }
-            },
-            {
-                id: 3, 
-                followed: false, 
-                fullName: "Kira Ne Lait", 
-                status: "I love pineapples <3", 
-                photoUrl: 'http://pm1.narvii.com/6967/4682fe603129c0b762d6f372ccc81f64581c978fr1-1024-768v2_uhq.jpg',
-                location: { 
-                    city: "Moscow",
-                    country: "Russia"
-                }
-            }
-        ]);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     let users = props.users.map(u => <User user={u} toggleFollow={props.toggleFollow} />);
