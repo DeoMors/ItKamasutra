9b424cbd08638b92a7348449e506f75e0208cc56
Maks <rezakoff.maxim@gmail.com>
2021-10-26 19:57:46 +0300

    Иммутабельность Users.jsx

--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 import User from './User/User';
 import styles from './Users.module.css'
 
+
 const Users = (props) => {
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+        
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     let users = props.users.map(u => <User user={u} toggleFollow={props.toggleFollow} />);
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {users}
         </div>
     )
