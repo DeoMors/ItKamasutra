d8a33f647246ced89b9851c29d65b07e487339fc
Maks <rezakoff.maxim@gmail.com>
2021-10-27 09:54:57 +0300

    Первая классовая компонента

--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -3,24 +3,25 @@ import React from 'react';
 import User from './User/User';
 import styles from './Users.module.css'
 
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-const Users = (props) => {
-        
-    const getUsers = () => {
-        if (props.users.length === 0) {
+        if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
         }
     }
-
-    let users = props.users.map(u => <User user={u} toggleFollow={props.toggleFollow} />);
-    return (
-        <div>
-            <button onClick={getUsers}>Get users</button>
-            {users}
-        </div>
-    )
+    
+    render() {
+        let users = this.props.users.map(u => <User user={u} toggleFollow={this.props.toggleFollow} />);
+        return (
+            <div>
+                {users}
+            </div>
+        );
+    }
 }
 
 export default Users;
