24eb8b96fe60eb99cee409198d503b9bec4139d5
Maks <rezakoff.maxim@gmail.com>
2021-11-06 22:26:24 +0300

    Обращаемся к апи из контейнерной компоненты

--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -1,42 +1,25 @@
-import * as axios from 'axios'
 import React from 'react';
-import User from './User/User';
 import styles from './Users.module.css'
+import User from './User/User';
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setTotalUsersCount(response.data.totalCount)
-            this.props.setUsers(response.data.items);
-        });
+let Users = (props) => {    
+    let users = props.users.map(u => <User user={u} toggleFollow={props.toggleFollow} />);
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageClick = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
-    };
-
-    render() {
-        let users = this.props.users.map(u => <User user={u} toggleFollow={this.props.toggleFollow} />);
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div>
-                {users}
-                {pages.map(p => {
-                    return <span className={p == this.props.currentPage && styles.selectedPage}
-                                 onClick={() => this.onPageClick(p)}>{p}</span>
-                })}
-            </div>
-        );
-    }
+    return (
+        <div>
+            {users}
+            {pages.map(p => {
+                return <span className={p == props.currentPage && styles.selectedPage}
+                             onClick={() => props.onPageClick(p)}>{p}</span>
+            })}
+        </div>
+    );
 }
 
 export default Users;
--- frontend/src/components/Users/UsersContainer.jsx
+++ frontend/src/components/Users/UsersContainer.jsx
@@ -1,6 +1,34 @@
+import axios from 'axios';
+import React from 'react';
 import { connect } from 'react-redux';
 import { setUsersActionCreator, toggleFollowActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator } from '../../redux/UsersReducer';
 import Users from './Users';
+
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setTotalUsersCount(response.data.totalCount)
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    onPageClick = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    };
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      onPageClick={this.onPageClick}
+                      users={this.props.users}
+                      toggleFollow={this.props.toggleFollow} />;
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -28,6 +56,4 @@ let mapDispatchToProps = (dispatch) => {
     };
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);;
