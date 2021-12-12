a3924833e8994e6f750d5f354bd233785776e73c
Maks <rezakoff.maxim@gmail.com>
2021-12-12 22:00:31 +0300

    Меняю фото и фон профиля

--- frontend/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ frontend/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -10,10 +10,10 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img src='https://www.ecomagazine.com/images/Newsletter/0_2019/Week_11-18-19/birdseyeview_ocean.jpg' width="100%"/>
+                <img className={styles.profileBackground} src='https://www.ecomagazine.com/images/Newsletter/0_2019/Week_11-18-19/birdseyeview_ocean.jpg' width="100%"/>
             </div>
-            <div className={styles.descriptionBlock}>
-                <img src={props.profile.photos.large} />
+            <div className={styles.description}>
+                <img className={styles.avatar} src={props.profile.photos.large} />
                 ava + desc
             </div>
         </div>
